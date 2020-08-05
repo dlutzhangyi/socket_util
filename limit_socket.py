b85369ae96d7eb2d01431c135d8d0a9b36fba78e
张一 <zhangyi61@meituan.com>
2020-08-05 10:30:26 +0800

    first commit

--- limit_socket.py
+++ limit_socket.py
@@ -1,8 +1,6 @@
 import tushare as ts
 import time
-import numpy as np
-from math import isnan
-
+import csv
 # 获取a股的所有股票数据
 def get_socket_data():
     socket_data = ts.get_stock_basics()
@@ -23,8 +21,11 @@ def get_socket_limit_number(socket_number):
     data = get_socket_hist_data(socket_number)
     limit_number = 0
     default_length = 10
-    if len(data["p_change"].values) < 10:
-        default_length = len(data["p_change"].values) 
+    try:
+        if len(data["p_change"].values) < 10:
+            default_length = len(data["p_change"].values) 
+    except:
+        print("获取p_change的values失败,股票代码为:",socket_number)        
     for i in range(default_length):
         if data["p_change"].values[i]>= 9.9:
             limit_number = limit_number + 1
@@ -34,15 +35,22 @@ def get_socket_limit_number(socket_number):
 
 # 获取a股所有上市的涨停板列表
 def get_limit_socket_data(sockets_dict):
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    file_name = "socket_limit_number_" + today +".csv"
+    with open(file_name,'wb') as f:
+        csv_write = csv.writer(f)
+        csv_head = ["股票代码","股票名","涨停次数"]
+        csv_write.writerow(csv_head)
+    
     for key in sockets_dict:
+        print(key)
         number = get_socket_limit_number(key)
         if number >= 2:
             print("股票代码:",key,"股票名:",sockets_dict[key],"涨停次数:",number)
+            content = [key,sockets_dict[key],str(number)]
+            csv_write.writerow(content)
+    f.close()
 
 if __name__ == "__main__":
     all_sockets = get_socket_data()
     get_limit_socket_data(all_sockets)
-    # number = get_socket_limit_number("300318") 
-    # print(number)
-    # print(get_socket_data())
-    # print(get_socket_hist_data("300318"))
